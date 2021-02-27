9068806653391abfa64ab99b836b0b35cf9a8391
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-27 01:03:21 -0600

    Update main.py
    Delete solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-
+"""
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,24 +13,3 @@ entrada = input()
 
 # Salidas
 print(salida)
-"""
-
-"""
-Comienza con vocal
-"""
-
-# Declaraciones
-VOCALES = "aeiouáéíóúü"
-
-# Entradas
-palabra = input("Escriba una palabra: ")
-
-# Proceso
-if len(palabra) > 0 and palabra[0].lower() in VOCALES:
-    comienza = "comienza"
-else:
-    comienza = "no comienza"
-respuesta = "'" + palabra + "' " + comienza + " con vocal"
-
-# Salidas
-print(respuesta)
